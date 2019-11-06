01c622c73734c1d7a3f18066325096e7b4d56b0a
Ayemere Odia <Odiadavid2@gmail.com>
2019-11-06 03:15:05 +0100

    Initial Commit

---
+++ Leggings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leggings
+//
+//  Created by Ayemere  Odia  on 06/11/2019.
+//  Copyright © 2019 Ayemere  Odia . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
